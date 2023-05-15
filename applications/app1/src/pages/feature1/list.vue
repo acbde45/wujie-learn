f00174ea97807736b9cf9000c44193702fdf4341
niuyx3 <niuyx3@sany.com.cn>
2023-05-15 09:55:19 +0800

    删除main2

--- applications/app1/src/pages/feature1/list.vue
+++ applications/app1/src/pages/feature1/list.vue
@@ -2,7 +2,7 @@
   <n-space vertical>
     <n-input placeholder="请输入" />
     <n-space>
-      <router-link to="/feature1/detail">
+      <router-link :to="app1DetailRoutePath">
         <n-button>详情</n-button>
       </router-link>
       <n-button @click="jumpToMainFeature1Detail">main feature1 详情</n-button>
@@ -13,6 +13,8 @@
 <script setup lang="ts">
 import { onActivated, onDeactivated } from "vue";
 import { NButton, NInput, NSpace } from "naive-ui";
+
+const app1DetailRoutePath = window.__POWERED_BY_WALLWORLD__ ? "/app1/feature1/detail" : "/feature1/detail";
 
 onActivated(() => {
   console.log("activated");
